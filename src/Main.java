9883fa3e6c35ee58e3a2b0079a8f3b10962bca56
Luann Henrique <LuannHenri@outlook.com>
2025-10-01 01:25:29 -0300

    adicionado a Tabuada

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Olá Git!");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("""
+                Bem vindo!
+                Escolha um numero para fazer a tabuada de 0 a 10
+                """);
+
+        int escolha;
+
+        while (!sc.hasNextInt()){
+            System.out.print("Entrada invalida! Digite um numero inteiro: ");
+            sc.next();
+            }
+        escolha = sc.nextInt();
+
+        for (int i = 0; i <= 10; i++) {
+            int calculo = escolha * i;
+            System.out.println(escolha + " X " + i + " = "+ calculo );
+        }
+        sc.close();
     }
 }
